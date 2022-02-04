5b4092628f4639f3ecf774cbda2320940550ea31
FishTox <48439275+apopoetic@users.noreply.github.com>
2022-02-05 00:18:20 +0530

    Update India 2020 earthquake map plot.R

--- India 2020 earthquake map plot.R
+++ India 2020 earthquake map plot.R
@@ -2,9 +2,8 @@ library(tidyverse)
 library(ggmap)
 library(lubridate)
 
-#load earthquake data
-eq_ <- read_csv("C:\\Users\\ankur\\OneDrive\\
-                R scripts\\R_Geocomputation\\eq.csv", na = c("", NA))
+#load earthquake data (from https://www.indiastat.com/table/meteorological-data/month-wise-earthquakes-detected-located-seismologi/1382679)
+eq_ <- read_csv("data.csv", na = c("", NA))
 #remove rows with NA
 eq <- eq_ %>% drop_na()
 #convert date column to as.Date
@@ -16,6 +15,6 @@ india <- get_stamenmap(bbox = c(left = 65.2, bottom = 5.8, right = 98.5, top = 3
 ggmap(india) + geom_point(data = eq, aes(x = long, y = lat, colour = month(date), alpha = 0.25)) +
   xlab("") + ylab("") + labs(title = "Month-wise earthquakes \n detected in India in the year 2020", caption = "Source: Ministry of Earth Sciences, Govt. of India") +
   theme(plot.title = element_text(size = 10))
-ggsave("C:\\Users\\ankur\\OneDrive\\R scripts\\R_Geocomputation")
+ggsave()
 
 
